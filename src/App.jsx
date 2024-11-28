2c7d78cb26281bf256a032c4f4638ddb70674397
Anees Ur Rehman <aneesliaqat555@gmail.com>
2024-11-28 22:55:12 +0500

    v

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
   const [length, setLength] = useState(7);
   const [numberAllowed, setNumberFun] = useState(false);
   const [charAllowed, setCharacterFun] = useState(false);
-  
-  
+
+
   const generatePassword = useCallback(() => {
     let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     let pass = ''
@@ -30,14 +30,14 @@ function App() {
 
   }, [setPassword, length, numberAllowed, charAllowed])
 
-  const copyPassword = useCallback(async () =>{
+  const copyPassword = useCallback(async () => {
     try {
       window.navigator.clipboard.writeText(password)
     } catch (error) {
       console.log(error)
     }
-  },[password])
-  
+  }, [password])
+
   useEffect(() => {
     generatePassword()
   }, [length, numberAllowed, charAllowed, setPassword])
@@ -46,7 +46,8 @@ function App() {
     <>
       <div className="flex justify-center place-content-center select-none">
         <div className="mx-auto w-3/5 p-5 min-h-60 max-h=96 mt-40 rounded-xl bg-slate-200">
-        <h1 className='mx-auto text-4xl font-bold text-gray-500 my-4 '>Password Generator</h1>
+          <h1 className='mx-auto text-4xl font-bold text-gray-500 my-4 '>Password Generator</h1>
+          <button onClick={() => generatePassword()} className='text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:hover:bg-blue-700'>Try Another one</button>
           <label htmlFor="default" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
           <div className="relative">
             <input type='search' id="default" className="block w-full text-2xl p-4 ps-10 text-gray-900 outline-none border-none rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
@@ -59,11 +60,11 @@ function App() {
               <label htmlFor="rang" className=' font-bold text-orange-600'> Range: {length}</label>
             </div>
             <div className=' flex gap-2 ml-2'>
-              <input type="checkbox" name="characters" id="char" onChange={() => setCharacterFun(prev => !prev)}/>
+              <input type="checkbox" name="characters" id="char" onChange={() => setCharacterFun(prev => !prev)} />
               <label htmlFor="char">Character</label>
             </div>
             <div className='flex gap-2 ml-2'>
-              <input type="checkbox" name="Number" id="number" onChange={() => setNumberFun(prev => !prev)}/>
+              <input type="checkbox" name="Number" id="number" onChange={() => setNumberFun(prev => !prev)} />
               <label htmlFor="number">Numbers</label>
             </div>
           </div>
